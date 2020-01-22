8e411ea7f86bc6780e0bb7145167d8cc8c46a800
Tom Doyle <tom.doyle.im@gmail.com>
2020-01-22 11:41:32 +0000

    comments

--- src/utilities/map.js
+++ src/utilities/map.js
@@ -1,7 +1,10 @@
 import { loadModules } from 'esri-loader';
 
 function initialiseMap(container, data) {
+  // emulates the behaviour of require.
+  // arcGIS is loaded initially on the page.
   loadModules(['esri/views/MapView', 'esri/WebMap', 'esri/layers/FeatureLayer'])
+  // having loaded the required modules, construct the map
     .then(([MapView, WebMap, FeatureLayer]) => {
     // then we load a web map from an id
       const map = new WebMap({
@@ -16,7 +19,7 @@ function initialiseMap(container, data) {
           map.add(layer);
         });
       }
-      // and we show that map in a container w/ id #viewDiv
+      // and we show that map in a container w/ id #mapouter
       // don't need to new this.
       MapView({ map, container });
     })
